28f35db748eb5166e1be00cbc1da8919d88d051a
dywudmin <mindu6424@naver.com>
2022-05-09 21:15:37 +0900

    20220509last

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render(request, 'base.html')
+    return render(request, 'mainapp/mainpage.html')
