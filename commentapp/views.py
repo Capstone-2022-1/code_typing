7f98296ec2f09d32405a75ef7d70f07eeaf5a066
hyejin <rkdgpwlsz@naver.com>
2022-05-16 23:44:33 +0900

    comment_deleteX

--- commentapp/views.py
+++ commentapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -24,3 +24,4 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('postapp:detail', kwargs={'pk': self.object.post.pk})
+
