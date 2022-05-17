5154ce2f3ccfb13a0ad4a16eb247137d0e42b9c5
hyejin <rkdgpwlsz@naver.com>
2022-05-17 21:46:47 +0900

    이미지수정완료

--- Codes/settings.py
+++ Codes/settings.py
@@ -145,5 +145,5 @@ LOGOUT_REDIRECT_URL = reverse_lazy('mainapp:mainpage')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-MEDIA_URL = 'media/'
-MEDIA_DIRS = [os.path.join(BASE_DIR, 'media'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
