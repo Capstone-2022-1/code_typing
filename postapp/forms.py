87719236143b15b92218727ed97020ab110be18d
dywudmin <mindu6424@naver.com>
2022-05-19 19:11:53 +0900

    글작성

--- postapp/forms.py
+++ postapp/forms.py
@@ -7,3 +7,9 @@ class PostCreationForm(ModelForm):
     class Meta:
         model = Post
         fields = ['category', 'post_title', 'image', 'post_content']
+        labels = {
+            'category': '카테고리',
+            'post_title': '제목',
+            'image': '이미지',
+            'post_content': '내용'
+        }
