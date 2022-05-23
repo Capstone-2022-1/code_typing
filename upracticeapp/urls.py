23c8238960eab2e0b5c72e76d096f765e4450836
dywudmin <mindu6424@naver.com>
2022-05-23 19:20:10 +0900

    upractice ing

--- upracticeapp/urls.py
+++ upracticeapp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from upracticeapp.views import upractice_first
+from upracticeapp.views import upractice_first, upractice_main
 
 app_name = 'upracticeapp'
 
 urlpatterns = [
-    path('', upractice_first, name='ufirstpractice'),
+    path('', upractice_main, name='mainupractice'),
+    path('ucreate/', upractice_first, name='ufirstpractice'),
 ]
--- upracticeapp/views.py
+++ upracticeapp/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from upracticeapp.models import Upractice
+
+def upractice_main(request):
+    return render(request, "upracticeapp/upractice_main.html")
 
 def upractice_first(request):
-    return render(request, 'upracticeapp/upractice_first.html')
+    try:
+        if request.method == "GET":
+            utitle = request.GET.get('utitle')
+            ucontent = request.GET.get('ucontent')
+            uresult = request.GET.get('uresult')
+            print(utitle, ucontent, uresult, len(ucontent))
+
+            upractice_data = Upractice()
+            upractice_data.upractice_title= utitle
+            upractice_data.upractice_content = ucontent
+            upractice_data.upractice_result = uresult
+            upractice_data.upractice_chnum = len(ucontent)
+            upractice_data.save()
+
+            context = {'utitle': utitle, 'ucontent': ucontent, 'uresult': uresult}
+
+            return render(request, "upracticeapp/upractice_main.html", context)
+
+    except Exception as identifier:
+        print(identifier)
+
+    return render(request, "upracticeapp/upractice_first.html")
