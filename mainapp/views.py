80bc0c50d01835339bc68f592515185e6e242165
hyejin <rkdgpwlsz@naver.com>
2022-06-04 18:31:25 +0900

    마이페이지게시글받아오기

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,8 +6,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
+from commentapp.models import Comment
 from mainapp.forms import AccountUpdateForm
+from postapp.models import Post
 
 
 def main_page(request):
@@ -19,10 +22,17 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('mainapp:mainpage')
     template_name = 'mainapp/signup.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'mainapp/detail.html'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        object_list = Post.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
+
 
 class AccountUpdateView(UpdateView):
     model = User
